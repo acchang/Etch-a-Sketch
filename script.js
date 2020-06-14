ec51173fc58c1f82341cb6f7ad0a1599972eeb28
acchang <acchang4@gmail.com>
2020-06-14 14:16:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -53,9 +53,9 @@ function skittlesMode() {
     }
 
 function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  const color = '#';
-  for (i = 0; i < 6; i++) {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
